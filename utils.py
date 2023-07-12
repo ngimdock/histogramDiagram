be6c4184c4ea7f8942c9787f86a2a94f5b0201ad
ngimdock <ngimdockzemfack@gmail.com>
2023-07-13 00:01:25 +0100

    Taille ( Bytes)

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
 def createHistogram(abscissaData, ordinateData, barColors):
-  plt.bar(abscissaData, ordinateData, color=barColors, zorder=2)
+  plt.bar(abscissaData, ordinateData, color=barColors, zorder=2, width=0.6)
   # plt.xlabel("Format Name")
   # plt.title("Formats by Size")
-  plt.ylabel("Payload size (B)")
+  plt.ylabel("Taille ( Bytes)")
   plt.xticks(rotation=90)
   plt.grid(axis="y", linestyle="dashed", linewidth=0.5, zorder=1)
   plt.xticks(fontsize=9, rotation=10)
