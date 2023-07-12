1fba6e9f88e68c51080312646c7581cd237bdf66
ngimdock <ngimdockzemfack@gmail.com>
2023-07-12 23:44:49 +0100

    add colors

--- firstHistogram.py
+++ firstHistogram.py
@@ -4,15 +4,15 @@ from utils import createHistogram
 
 firstDiagramDataset = [
   Format(FormatName.ASN_1.value, 44, "#BBB771"),
-  Format(FormatName.APACHE_AVRO.value, 56, "#CE5D5B"),
+  Format(FormatName.APACHE_AVRO.value, 56, "#5B3758"),
   Format(FormatName.MICROSOFT_BOND.value, 77, "#F4A361"),
   Format(FormatName.CAPTN_PROTO.value, 83, "#208CF6"),
   Format(FormatName.FLAT_BUFFERS.value, 208, "#46B073"),
-  Format(FormatName.PROCOLE_BUFFERS.value, 64, "#111B21"),
+  Format(FormatName.PROCOLE_BUFFERS.value, 64, "#CE5D5B"),
   Format(FormatName.APACHE_THRIFT.value, 64, "#5C6468"),
 ]
 
-# Create lists of format names, sizes, and bar colors
+
 format_names = [format.name for format in firstDiagramDataset]
 sizes = [format.size for format in firstDiagramDataset]
 bar_colors = [format.barColor for format in firstDiagramDataset]
--- secondDiagram.py
+++ secondDiagram.py
@@ -3,15 +3,15 @@ from data import FormatName
 from utils import createHistogram
 
 firstDiagramDataset = [
-  Format(FormatName.BSON.value, 223, "#BBB771"),
+  Format(FormatName.BSON.value, 223, "#F9ADA0"),
   Format(FormatName.CBOR.value, 118, "#CE5D5B"),
-  Format(FormatName.FLEX_BUFFERS.value, 176, "#F4A361"),
-  Format(FormatName.MASSAGE_PACK.value, 118, "#208CF6"),
-  Format(FormatName.SMILES.value, 127, "#208CF6"),
-  Format(FormatName.UB_JSON.value, 151, "#208CF6"),
+  Format(FormatName.FLEX_BUFFERS.value, 176, "#208CF6"),
+  Format(FormatName.MASSAGE_PACK.value, 118, "#5C6468"),
+  Format(FormatName.SMILES.value, 127, "#46B073"),
+  Format(FormatName.UB_JSON.value, 151, "#BBB771"),
 ]
 
-# Create lists of format names, sizes, and bar colors
+
 format_names = [format.name for format in firstDiagramDataset]
 sizes = [format.size for format in firstDiagramDataset]
 bar_colors = [format.barColor for format in firstDiagramDataset]
